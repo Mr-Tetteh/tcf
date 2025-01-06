de8d0f9def0095ece639eacfa231cdfb02f6e8d1
Mr. Tetteh <danielstay73@gmail.com>
2025-01-06 22:21:01 +0000

    about page done

--- routes/user_route.php
+++ routes/user_route.php
@@ -1,4 +1,8 @@
 <?php
 use Illuminate\Support\Facades\Route;
+Route::prefix('users')->group(function () {
+    Route::get('/', \App\Livewire\User\Home::class)->name('home');
+    Route::get('/about', \App\Livewire\User\About::class)->name('about');
 
-Route::get('/', \App\Livewire\User\Home::class)->name('home');
+
+});
