fb98be35078709b5f86e3dd3e04cd37beac9014b
Mr. Tetteh <danielstay73@gmail.com>
2025-01-16 03:12:51 +0000

    gallery

--- routes/admin_route.php
+++ routes/admin_route.php
@@ -8,4 +8,5 @@ Route::prefix('admin')->group(function () {
     Route::get('/admin_users', \App\Livewire\Admin\AdminUsers::class)->name('admin.admin_users');
     Route::get('/register_member', \App\Livewire\Admin\RegisterMember::class)->name('admin.register_member');
     Route::get('/sermon', \App\Livewire\Admin\Sermon::class)->name('admin.sermon');
+    Route::get('gallery', \App\Livewire\Admin\Gallery::class)->name('admin.gallery');
 });
