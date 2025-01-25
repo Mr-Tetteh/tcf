979001bc5b8d83c57130e1b37c40b302fcab4201
Mr. Tetteh <danielstay73@gmail.com>
2025-01-25 23:27:26 +0000

    auth works

--- routes/admin_route.php
+++ routes/admin_route.php
@@ -4,16 +4,18 @@ use App\Livewire\Admin\FamilyGathering;
 use App\Livewire\Admin\RecordManagement;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware('auth')->group(function () {
 
-Route::prefix('admin')->group(function () {
-    Route::get('/dashboard', \App\Livewire\Admin\Dashboard::class)->name('admin.dashboard');
-    Route::get('/admin_users', \App\Livewire\Admin\AdminUsers::class)->name('admin.admin_users');
-    Route::get('/register_member', \App\Livewire\Admin\RegisterMember::class)->name('admin.register_member');
-    Route::get('/sermon', \App\Livewire\Admin\Sermon::class)->name('admin.sermon');
-    Route::get('gallery', \App\Livewire\Admin\Gallery::class)->name('admin.gallery');
-    Route::get('/study_material', \App\Livewire\Admin\StudyMaterial::class)->name('admin.study_material');
-    Route::get('family_gathering', FamilyGathering::class)->name('admin.family_gathering');
-    Route::get('record_management', RecordManagement::class)->name('admin.record_management');
-    Route::get('/finance', \App\Livewire\Admin\Finance::class)->name('admin.finance');
-    Route::get('/online_translations', \App\Livewire\Admin\Online::class)->name('admin.online');
+    Route::prefix('admin')->group(function () {
+        Route::get('/dashboard', \App\Livewire\Admin\Dashboard::class)->name('admin.dashboard');
+        Route::get('/admin_users', \App\Livewire\Admin\AdminUsers::class)->name('admin.admin_users');
+        Route::get('/register_member', \App\Livewire\Admin\RegisterMember::class)->name('admin.register_member');
+        Route::get('/sermon', \App\Livewire\Admin\Sermon::class)->name('admin.sermon');
+        Route::get('gallery', \App\Livewire\Admin\Gallery::class)->name('admin.gallery');
+        Route::get('/study_material', \App\Livewire\Admin\StudyMaterial::class)->name('admin.study_material');
+        Route::get('family_gathering', FamilyGathering::class)->name('admin.family_gathering');
+        Route::get('record_management', RecordManagement::class)->name('admin.record_management');
+        Route::get('/finance', \App\Livewire\Admin\Finance::class)->name('admin.finance');
+        Route::get('/online_translations', \App\Livewire\Admin\Online::class)->name('admin.online');
+    });
 });
