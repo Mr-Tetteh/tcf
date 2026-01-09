b4bbafc285434e32ec22bc7fc48431f5a9ef9af8
Mr.Tetteh <danielstay73@gmail.com>
2026-01-09 22:35:51 +0000

    grouped data by day

--- app/Livewire/Admin/FamilyGatheringAllYears.php
+++ app/Livewire/Admin/FamilyGatheringAllYears.php
@@ -11,13 +11,31 @@ class FamilyGatheringAllYears extends Component
 {
     #[Layout('layout.admin.partials.website-base-admin')]
 
-    public function render()
-    {
-        $familiesByYear = FamilyGathering::latest()->
-            get()
-            ->groupBy('year');
-        $number_of_registered_members = \App\Models\FamilyGathering::whereYear('created_at', Carbon::now()->year)->count();
-        $number_of_registered_members_without_amount_paid =  FamilyGathering::where('amount_paid','0.00')->count();
-        return view('livewire.admin.family-gathering-all-years', compact('familiesByYear', 'number_of_registered_members', 'number_of_registered_members_without_amount_paid'));
-    }
+  public function render()
+{
+    $familiesByYear = FamilyGathering::latest()
+        ->get()
+        ->groupBy('year')
+        ->map(function ($familiesInYear) {
+            return $familiesInYear->groupBy(function ($family) {
+                return Carbon::parse($family->created_at)->format('Y-m-d');
+            });
+        });
+
+    $number_of_registered_members =
+        FamilyGathering::whereYear('created_at', Carbon::now()->year)->count();
+
+    $number_of_registered_members_without_amount_paid =
+        FamilyGathering::where('amount_paid', '0.00')->count();
+
+    return view(
+        'livewire.admin.family-gathering-all-years',
+        compact(
+            'familiesByYear',
+            'number_of_registered_members',
+            'number_of_registered_members_without_amount_paid'
+        )
+    );
+}
+
 }
